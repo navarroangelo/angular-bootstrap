f612dfa08fe4e7536d6ec0381f8c5d4c7fe1b2ba
Angelo Navarro <marcelxangelo@gmail.com>
2024-12-02 13:12:07 +0800

    Initial Draft - Angular Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// IMPORTING THE COMPONENTS
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FeaturedComponent } from './components/featured/featured.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    FeaturedComponent,
+    GalleryComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-bootstrap';
